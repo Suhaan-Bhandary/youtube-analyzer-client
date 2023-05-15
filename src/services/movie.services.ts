91850abc8f997742f2d3a9ab40396fcb2adcd6b6
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-15 14:26:41 +0530

    feat: Integrated real api to the application

--- src/services/movie.services.ts
+++ src/services/movie.services.ts
@@ -1,24 +1,12 @@
+import axios from 'axios';
 import { MovieData } from '../types/movie.types';
-import fakeRequest from './fakeRequest';
+import { BASE_API_URL } from './serverConfig';
 
 const getMovieRecommendation = (movieTitle: string, movieGenre: string) => {
-  const expectedResponse: MovieData = {
-    movies: [
-      { date: '2020', description: 'This is a movie', title: 'movie' },
-      { date: '2021', description: 'This is a movie 2', title: 'movie2' },
-    ],
-  };
+  const url = BASE_API_URL + '/api/movie';
+  const data = { movieTitle, movieGenre };
 
-  return fakeRequest(
-    { movieTitle, movieGenre },
-    expectedResponse,
-    false,
-  ) as Promise<MovieData>;
-
-  //   const url = BASE_API_URL + '/api/movie-recommendation';
-  //   const data = { movieTitle };
-
-  //   return axios.post<MovieData>(url, data).then((response) => response.data);
+  return axios.post<MovieData>(url, data).then((response) => response.data);
 };
 
 export { getMovieRecommendation };
